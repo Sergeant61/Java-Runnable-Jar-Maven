646875b411677089bad82664fe02ae73771e302d
mustafa_2 <mustafa_2@mustafaergan>
2017-05-04 23:21:54 +0300

    projenin ilk halini

---
+++ src/com/mustafaergan/pack1/Islem.java
@@ -0,0 +1,16 @@
+package com.mustafaergan.pack1;
+
+import com.mustafaergan.pack2.Nesne;
+
+public class Islem {
+
+	public static void main(String[] args) {
+		
+		Nesne nesne  = new Nesne();
+		
+		System.out.println(nesne.getVeri());
+		
+	}
+	
+	
+}
---
+++ src/com/mustafaergan/pack2/Nesne.java
@@ -0,0 +1,15 @@
+package com.mustafaergan.pack2;
+
+public class Nesne {
+	
+	String veri = "Merhaba Dunya";
+	
+	public void setVeri(String veri) {
+		this.veri = veri;
+	}
+	
+	public String getVeri() {
+		return veri;
+	}
+
+}
